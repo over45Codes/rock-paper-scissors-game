ba080dc04d4255205c639e6a06e254cfde513e8e
Grace Castaneda <admin@Mac-mini-de-mac.local>
2020-10-29 18:55:06 -0700

    commit "added to js scrip"

---
+++ script.js
@@ -0,0 +1,18 @@
+//console.log("script was loaded!");
+//document.addEventListener("click", function (e) {
+//console.log(e);
+//});
+
+const selectionItem = document.querySelectorAll("rock", "paper", "scissors");
+console.log(selectionItem);
+
+//   selectionItem.addEventListener("click", function () {
+//   console.log("clicked!!!");
+//  });
+
+//const selectionItem = document.getElementById("paper");
+//console.log(selectionItem);
+
+//selectionItem.addEventListener("click", function () {
+//console.log("clicked!!!");
+//});
