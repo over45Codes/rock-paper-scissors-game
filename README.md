a31e0e8a4a5d24bfd45722819086d9420a58be19
Grace Castaneda <griseldacastaneda2013@gmail.com>
2020-11-14 22:11:43 -0800

    commit to master 'final edits to JS & README files'

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Vanilla JavaScript Rock Paper Scissors App Game
 
-Rock-Paper-Scissors application built in JavaScript, HTML and CSS
+Rock-Paper-Scissors application game built in JavaScript, HTML and CSS
 
 Try it [here!](https://gracii.github.io/rock-paper-scissors-game/)
 
@@ -8,7 +8,7 @@ Try it [here!](https://gracii.github.io/rock-paper-scissors-game/)
 
 ## Summary
 
-This project was a great challenge because, I needed to use arrays, functions, MathRandom functions! Also, I needed to find creative ways to increase the score count in the web browser! It was an a great way to use CSS Styling and get a bit creative with colors and images, Last but not least in was a challenge to code the logic that would determine the outcome of all of the different variations that would be the outcome of each game.
+This Rock paper scissors is a classic game with a very simple rule set. This project was a great challenge because, I needed to use arrays, functions, MathRandom functions! Also, I needed to find creative ways to increase the score count in the web browser! It was an a great way to use CSS Styling which included CSS grid and flexbox to layout game elements and got a bit creative with colors and images. I learn how to simplify complex game logic and write clean ES6 code. Last but not least in was a challenge to code the logic that would determine the outcome of all of the different variations that would be the outcome of each game by creating an AI to play against.
 
 ## Author
 
--- script.js
+++ script.js
@@ -2,27 +2,16 @@ let playerScore = 0;
 let computerScore = 0;
 
 const playerChoices = document.querySelectorAll("selection-choice");
-
 const displayResults = document.querySelector(".displayResults");
 
-function getPlayerChoice() {
-  const rock = document.getElementById("rock");
-  rock.addEventListener("click", function () {
-    showResults("rock");
-    playerChoices.textContent = "Player picked rock";
-  });
-}
+const rock = document.getElementById("rock");
+rock.addEventListener("click", () => showResults("rock"));
+
 const paper = document.getElementById("paper");
-paper.addEventListener("click", function () {
-  showResults("paper");
-  playerChoices.textContent = "Player picked paper";
-});
+paper.addEventListener("click", () => showResults("paper"));
 
 const scissors = document.getElementById("scissors");
-scissors.addEventListener("click", function () {
-  showResults("scissors");
-  playerChoices.textContent = "Player picked scissors";
-});
+scissors.addEventListener("click", () => showResults("scissors"));
 
 function getComputerChoice() {
   const getComputerChoiceArray = ["rock", "paper", "scissors"];
@@ -31,18 +20,11 @@ function getComputerChoice() {
   return computerChose;
 }
 
-getPlayerChoice();
-
 function win(userChoice, computerChoice) {
   const updatePlayerScore = document.getElementById("player-scores");
   playerScore++;
   updatePlayerScore.textContent = playerScore;
-  const winner = (displayResults.textContent =
-    " Player chose " +
-    userChoice +
-    ". Computer chose " +
-    computerChoice +
-    ". You win! ");
+  const winner = (displayResults.textContent = `Player chose ${userChoice}. Computer chose ${computerChoice}. You win!`);
 
   return winner;
 }
@@ -51,23 +33,14 @@ function lose(userChoice, computerChoice) {
   const updateComputerScore = document.getElementById("computer-scores");
   computerScore++;
   updateComputerScore.textContent = computerScore;
-  const loser = (displayResults.textContent =
-    " Player chose " +
-    userChoice +
-    ". Computer chose " +
-    computerChoice +
-    ". You lose! ");
+  const loser = (displayResults.textContent = `Player chose ${userChoice}. Computer chose ${computerChoice}.
+  You lose!`);
 
   return loser;
 }
 
 function tie(userChoice, computerChoice) {
-  const tied = (displayResults.textContent =
-    " Player chose " +
-    userChoice +
-    ". Computer chose " +
-    computerChoice +
-    ". It's a tie ! ");
+  const tied = (displayResults.textContent = `Player chose ${userChoice}. Computer chose ${computerChoice}. It's a tie !`);
 
   return tied;
 }
@@ -79,19 +52,19 @@ function showResults(userChoice) {
     case "scissorspaper":
     case "paperrock":
       win(userChoice, computerChoice);
-      console.log("You win");
+
       break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
       lose(userChoice, computerChoice);
-      console.log("You lose");
+
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
       tie(userChoice, computerChoice);
-      console.log("It's a tie.");
+
       break;
   }
 }
